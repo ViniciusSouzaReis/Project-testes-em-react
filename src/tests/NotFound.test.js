b9c6ce59f75df3bcd1f82d57a43f1b1b9946450a
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-16 15:54:06 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const verifyText = screen.getByRole('heading', {
+        name: /Page requested not found/i, level: 2 });
+      expect(verifyText).toBeInTheDocument();
+    });
+
+  test('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const getImg = screen.getByRole('img', { name: /Pikachu crying because/i });
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(getImg.src).toBe(imgSrc);
+  });
+});
