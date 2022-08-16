89c634dbb08fe1113a07ad815969a9d93b5b95b2
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-16 15:44:07 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Exiba na tela a mensagem "No favorite pokemon found" caso não tenha pokemons',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+
+      const verifyText = screen.getByText(/No favorite pokemon found/i);
+      expect(verifyText).toBeInTheDocument();
+    });
+});
